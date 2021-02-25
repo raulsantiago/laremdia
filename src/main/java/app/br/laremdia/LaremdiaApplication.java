e8456d649114c88a4228e6f04635095a8c24dc6c
f950250 <f950250@mail.caixa>
2021-02-25 17:35:40 -0300

    Initial commit

---
+++ src/main/java/app/br/laremdia/LaremdiaApplication.java
@@ -0,0 +1,13 @@
+package app.br.laremdia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaremdiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaremdiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/br/laremdia/LaremdiaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.br.laremdia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaremdiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
