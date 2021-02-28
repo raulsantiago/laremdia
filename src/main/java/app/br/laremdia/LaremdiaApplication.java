efb4a5092fc4fcc3faf80c4ec315dadf114298f6
Raul Santiago <raulsantiago@id.uff.br>
2021-02-28 00:10:28 -0300

    comentar inserir

--- src/main/java/app/br/laremdia/LaremdiaApplication.java
+++ src/main/java/app/br/laremdia/LaremdiaApplication.java
@@ -13,31 +13,31 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class LaremdiaApplication {
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			@Autowired LoginProfissionalRepository repository, LoginClienteRepository repositoryCli){
-		return args -> {
-			LoginProfissional loginProfissional = new LoginProfissional();
-			loginProfissional.setNome("Jaq");
-			loginProfissional.setEmail("jaq@gmail.com");
-			loginProfissional.setSenha("123");
-			loginProfissional.setAtivo(true);
-			repository.save(loginProfissional);
-
-			LoginCliente loginCliente = new LoginCliente();
-			loginCliente.setNome("Marcia");
-			loginCliente.setEmail("marcia@gmail.com");
-			loginCliente.setSenha("123");
-			loginCliente.setEndereco("Rua Rafa Audax");
-			loginCliente.setNumero("1005");
-			loginCliente.setBairro("Laranjeiras");
-			loginCliente.setEstado("RJ");
-			loginCliente.setMunicipio("Rio de Janeiro");
-			loginCliente.setAtivo(true);
-			repositoryCli.save(loginCliente);
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			@Autowired LoginProfissionalRepository repository, LoginClienteRepository repositoryCli){
+//		return args -> {
+//			LoginProfissional loginProfissional = new LoginProfissional();
+//			loginProfissional.setNome("Jaq");
+//			loginProfissional.setEmail("jaq@gmail.com");
+//			loginProfissional.setSenha("123");
+//			loginProfissional.setAtivo(true);
+//			repository.save(loginProfissional);
+//
+//			LoginCliente loginCliente = new LoginCliente();
+//			loginCliente.setNome("Marcia");
+//			loginCliente.setEmail("marcia@gmail.com");
+//			loginCliente.setSenha("123");
+//			loginCliente.setEndereco("Rua Rafa Audax");
+//			loginCliente.setNumero("1005");
+//			loginCliente.setBairro("Laranjeiras");
+//			loginCliente.setEstado("RJ");
+//			loginCliente.setMunicipio("Rio de Janeiro");
+//			loginCliente.setAtivo(true);
+//			repositoryCli.save(loginCliente);
+//
+//		};
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaremdiaApplication.class, args);
